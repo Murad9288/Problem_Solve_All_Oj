c4a409f6fb5cab33b4a785a746e10a45505abb52
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-10-15 23:19:33 +0600

    Update Find the Runner-Up Score! solveing.py

--- HackerRank.com/Find the Runner-Up Score! solveing.py
+++ HackerRank.com/Find the Runner-Up Score! solveing.py
@@ -1,10 +1,15 @@
+# 1st System.
+
 n = int(input())
 arr = list(map(int, input().split()))
 z = max(arr)
 while max(arr) == z:
     arr.remove(max(arr))
 print(max(arr))
-'''n = int(input())
+
+# Second System.
+'''
+n = int(input())
 arr = list(map(int, input().split()))
 zes = max(arr)
 i=0
@@ -12,4 +17,6 @@ while(i<n):
     if zes ==max(arr):
         arr.remove(max(arr))
     i+=1
-print(max(arr))'''
+print(max(arr))
+
+'''
