be547728d6e6d72b0f6136233a5db35022785dc7
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-11-14 21:11:00 +0600

    Update Problem - 1006.py

--- Uri online judge.com/Problem - 1006.py
+++ Uri online judge.com/Problem - 1006.py
@@ -1,8 +1,7 @@
-while True:
-    A = float(input())
-    B = float(input())
-    C = float(input())
-    MEDIA = (A+B+C)/3
-    print('MEDIA = %0.1f'%MEDIA)
+A = float(input())
+B = float(input())
+C = float(input())
+MEDIA = (A+B+C)/3
+print('MEDIA = %0.1f'%MEDIA) 
 
 
