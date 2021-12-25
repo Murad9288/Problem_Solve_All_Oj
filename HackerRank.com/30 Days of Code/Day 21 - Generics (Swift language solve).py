da71d80bf824d491e6ed5cce9fcf842029940a56
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2021-12-25 12:37:16 +0600

    Add files via upload

---
+++ HackerRank.com/30 Days of Code/Day 21 - Generics (Swift language solve).py
@@ -0,0 +1,38 @@
+/**
+*  Language: Swift
+**/
+
+import Foundation
+
+struct Printer<T> {
+	/**
+	*    Name: printArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+	
+	// Write your code here
+    
+    func printArray(array: [T]) {
+            for i in array {
+                print(i)
+            }
+    }
+    
+}
+
+
+var n = Int(readLine()!)!
+var intArray = Array(repeating: 0, count: n);
+for i in 0...n - 1 {
+	intArray[i] = Int(readLine()!)!;
+}
+
+n = Int(readLine()!)!
+var stringArray = Array(repeating: "", count: n);
+for i in 0...n - 1 {
+	stringArray[i] = readLine()!;
+}
+
+Printer<Int>().printArray(array: intArray)
+Printer<String>().printArray(array: stringArray)
