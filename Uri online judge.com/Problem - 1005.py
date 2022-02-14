e51654b9479148caa17c2e05fbf4f6d8fb4befdd
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2022-02-14 16:14:10 +0600

    Update Problem - 1005.py

--- Uri online judge.com/Problem - 1005.py
+++ Uri online judge.com/Problem - 1005.py
@@ -1,6 +1,4 @@
-A = float(input())
-B = float(input())
-X = (A+B)
-Y = X/2
-print('MEDIA = %0.5f'%Y)
-
+first_floating_number = float(input())
+second_floating_number = float(input())
+avarege = ((first_floating_number*3.5) + (second_floating_number*7.5)) / (3.5+7.5)
+print("MEDIA = %0.5f"%avarege)
