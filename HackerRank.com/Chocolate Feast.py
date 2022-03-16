e91b2b526205f717736be7f7ad041e094cd731b7
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2022-03-16 23:50:23 +0600

    Update Chocolate Feast.py

--- HackerRank.com/Chocolate Feast.py
+++ HackerRank.com/Chocolate Feast.py
@@ -1,22 +1,30 @@
+* Bismillah_hirrah_manirharim *
+# My name is MD Murad Hossain.
+# My Gmail: muradhossainm01gmail.com
+
+
+
 # Swift Language Solution Code:
-'''
+
 let t = Int(readLine()!)!
 for _ in 1...t{
-let arr = readLine()!.split(separator: " ").map { Int($0)!}
-let n = arr[0]
-let c = arr[1]
-let m = arr[2]
-var res = n/c
-var w = res
-while w>=m{
-let k = w/m
-res += k
-w = k+(w%m)
+    let arr = readLine()!.split(separator: " ").map { Int($0)!}
+    let n = arr[0]
+    let c = arr[1]
+    let m = arr[2]
+    var res = n/c
+    var w = res
+    while w>=m{
+        let k = w/m
+        res += k
+        w = k+(w%m)
+    }
+    print(res)
 }
-print(res)
-}
-'''
+
+
 # Python Solution Code:
+
 '''
 for _ in range(int(input())):
     n,c,m = map(int,input().split())
@@ -28,3 +36,9 @@ for _ in range(int(input())):
         w = k+(w%m)
     print(n)
 '''
+
+
+
+
+
+
