e14b42006873d4e111328879919d982f2f865dc4
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2022-11-25 22:55:44 +0600

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -2,3 +2,28 @@
 
 print("Hello world")
 
+# Namota
+
+n = int(input())
+for i in range(1,11):
+  print("%d x %d = %d"%(n,i,i*n))
+  
+  
+  # output:
+  
+  '''
+  n  = 10
+  
+  10 x 1 = 10
+  10 x 2 = 20
+  10 x 3 = 30
+  10 x 4 = 40
+  10 x 5 = 50
+  10 x 6 = 60
+  10 x 7 = 70
+  10 x 8 = 80
+  10 x 9 = 90
+  10 x 10 = 100
+  
+  
+  '''
