334fd2fe260682c4b408829476eae7470b81ed7b
MD Murad Hossain <mdmuradhossainmiraj@gmail.com>
2022-11-26 01:01:17 +0600

    AD

--- DimikOj.com/(AC) Problem - 11 (গৌণিক - ফ্যাক্টরিয়াল).py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for i in range(T):
-        N = int(input())
-        factorial = 1
-        for j in range(1,N+1):
-                factorial = factorial*j
-        print(factorial)
