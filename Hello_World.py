cffc0e9553b06b8c0b725220e724294a8e1bfe72
MD Murad Hossain <81400169+Murad9288@users.noreply.github.com>
2022-11-26 03:21:27 +0600

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -303,18 +303,6 @@ nt("1st Character")
 //print(tuples.second)
 //
 
-// MARK: Enumerations
-
-//let result = "failure"
-//let result2 = "failed"
-//let result3 = "fail"
-//enum Result {
-//    case success
-//    case failure
-//}
-//let res = Result.failure
-//print(res)
-
 
 
 
