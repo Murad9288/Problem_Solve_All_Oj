2c3e3af464cfd9e9eeaa380a3af714e4cf62d56d
MD Murad Hossain <mdmuradhossainmiraj@gmail.com>
2022-11-27 02:36:12 +0600

    delete the
    Co-Authored-By: MD Miraz Hossain <118871519+Programmer1245@users.noreply.github.com>

--- DimikOj.com/Loop.py
+++ DimikOj.com/Loop.py
@@ -170,28 +170,6 @@ print(a,b,c)'''
 
 # Two list to one Dictionary
 
-'''
-one_list = [12,3,4]
-two_list = ["Mango","Orange","Banana"]
-dictionary = {}
-for i in range(len(one_list)):
-    dictionary[two_list[i]] = one_list[i]
-print(dictionary)
-
-'''
-for i in range(int(input())):
-    def twos_comp(val, bits):
-        """compute the 2's complement of int value val"""
-        if (val & (1 << (bits +1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-            val = val - (1 << bits)        # compute negative value
-        return val
-    a = int(input())
-    print(twos_comp(a,128))
-
-
-
-
-
 
 
 
